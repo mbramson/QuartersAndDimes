e3f86dcb126506cc883653e2ddc937e7bb041ec3
mbramson <mathewbramson@gmail.com>
2016-03-28 13:43:52 -0400

    implement circle_mod

--- lib/quarters_and_dimes.ex
+++ lib/quarters_and_dimes.ex
@@ -1,3 +1,26 @@
 defmodule QuartersAndDimes do
-  
+  def step(points) do
+    []
+  end
+
+  def reposition(first, last) do
+    if first < last do
+      (first + last) / 2
+    else
+      n = reposition(first, last + 360)
+      if n >= 360 do
+        n = n - 360
+      end
+      n
+    end
+  end
+
+  def circle_mod(n) do
+    cond do
+      n < 0 -> circle_mod(n + 360)
+      n >= 360 -> circle_mod(n - 360)
+      true -> n
+    end
+  end
+
 end
--- test/quarters_and_dimes_test.exs
+++ test/quarters_and_dimes_test.exs
@@ -1,3 +1,5 @@
+import QuartersAndDimes
+
 defmodule QuartersAndDimesTest do
   use ExUnit.Case
   doctest QuartersAndDimes
@@ -5,4 +7,41 @@ defmodule QuartersAndDimesTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "that step returns a list" do
+    assert is_list step []
+  end
+
+  # reposition tests
+
+  test "that reposition returns a point between two points" do
+    assert reposition(3, 7) == 5
+  end
+
+  test "that reposition returns a point between two points and wraps" do
+    assert reposition(270, 10) == 320
+  end
+
+  test "that reposition returns a point between 359 and 1" do
+    assert reposition(359, 1) == 0
+  end
+
+  # float_mod tests
+
+  test "that circle_mod returns same number if less than 360" do
+    assert circle_mod(200) == 200
+  end
+
+  test "that circle_mod returns number minus 360" do
+    assert circle_mod(460) == 100
+  end
+
+  test "that circle_mod returns number less than 360 for large n" do
+    assert circle_mod(820) == 100
+  end
+
+  test "that circle_mod returns number between 0 and 360 when negative" do
+    assert circle_mod(-90) == 270
+  end
+
 end
